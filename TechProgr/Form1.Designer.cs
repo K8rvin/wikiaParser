4296de716ca85b92c4ca0c28b5ca97c215e4a8d1
Андрей Мохов <C:\Users\\AppData\Roaming\The Bat!>
2017-05-15 10:13:45 +0300

    Initial commit.

---
+++ TechProgr/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace TechProgr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 34);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "гарри";
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(41, 88);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 1;
+            this.buttonFind.Text = "Поиск";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(-4, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(483, 290);
+            this.textBox2.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 117);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(483, 312);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(475, 286);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "html";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(475, 286);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "ParsingResult";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(-4, 0);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(483, 291);
+            this.textBox3.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "http://ru.elderscrolls.wikia.com — The Elder Scrolls Wiki (энциклопедия серии игр" +
+                " «The Elder Scrolls»)",
+            "http://ru.terraria.wikia.com — Террария вики (энциклопедия игры «Terraria»)",
+            "http://ru.darksouls.wikia.com — Dark Souls Wiki (энциклопедия серии игр «Dark Sou" +
+                "ls»)",
+            "http://ru.fallout.wikia.com — Убежище (энциклопедия серии игр «Fallout»)",
+            "http://ru.harrypotter.wikia.com — Гарри Поттер вики (энциклопедия о вселенной «Га" +
+                "рри Поттера»)",
+            "http://ru.starwars.wikia.com — Вукипедия (энциклопедия о «Звёздных войнах»)",
+            "http://ru.dont-starve.wikia.com — Don\'t Starve Wiki (энциклопедия игры «Don\'t Sta" +
+                "rve»)",
+            "http://ru.bindingofisaac.wikia.com — The Binding of Isaac вики (энциклопедия сери" +
+                "и игр «The Binding of Isaac»)",
+            "http://vedmak.wikia.com — Ведьмак Вики (энциклопедия вселенной «Ведьмака»)",
+            "http://ru.gameofthrones.wikia.com — Игра Престолов Вики (энциклопедия сериала «Иг" +
+                "ра Престолов»)",
+            "http://ru.007-pedia.wikia.com — о киноэпопее о Джеймсе Бонде"});
+            this.comboBox1.Location = new System.Drawing.Point(41, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(454, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 441);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TechProgr/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace TechProgr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 4;            
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            String Text = textBox1.Text;
+            String Site="";
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: Site = "http://ru.elderscrolls.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 1: Site = "http://ru.terraria.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 2: Site = "http://ru.darksouls.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 3: Site = "http://ru.fallout.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 4: Site = "http://ru.harrypotter.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 5: Site = "http://ru.starwars.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 6: Site = "http://ru.dont-starve.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 7: Site = "http://ru.bindingofisaac.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 8: Site = "http://vedmak.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 9: Site = "http://ru.gameofthrones.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;
+                case 10: Site= "http://ru.007-pedia.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Search?query="; break;                
+            }                
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Site + Text);
+            request.Method = "GET";
+            request.Accept = "application/json";            
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            StringBuilder output = new StringBuilder();
+            output.Append(reader.ReadToEnd());
+
+            textBox2.Text = output.ToString();
+            textBox3.Text = "";
+
+            Parser p = new Parser();
+            textBox3.Text = p.Parsing(output.ToString(), "data-pos=(.+)");
+        }        
+    }
+
+    public class Parser
+    {
+        public String Parsing(string Http, string regexS)
+        {
+            String result = "";
+            Regex Regex = new Regex(regexS);
+            Match match = Regex.Match(Http);
+            // отображаем все совпадения
+            while (match.Success)
+            {
+                // Т.к. мы выделили в шаблоне одну группу (одни круглые скобки),
+                // ссылаемся на найденное значение через свойство Groups класса Match
+                result += RemoveDetails(match.Groups[0].Value);                
+                // Переходим к следующему совпадению
+                match = match.NextMatch();
+            }
+            return result;
+        }
+
+        public String RemoveDetails(String match)
+        {            
+            return match.Replace("</a>", "").Replace("</li>", "").Replace("data-pos=", "").
+                    Replace("</b><!--", "").Replace("comment to remove whitespace", "[Лучшие статьи]").
+                    Replace("data-event=\"search_click_match\"", "[Точное совпадение]").Replace(">", " ") + Environment.NewLine;
+        }
+    }
+
+    [TestFixture]
+    public class ParserTestCase
+    {
+        [Test]
+        public void testTrue()
+        {
+            Parser p = new Parser();
+            string HTML = "";
+            Assert.AreEqual("", p.Parsing(HTML, "data-pos=(.+)"));            
+        }
+
+        [Test]
+        public void testFalse()
+        {
+            Parser p = new Parser();
+            string HTML = "";
+            Assert.AreEqual("1", p.Parsing(HTML, "data-pos=(.+)"));            
+        }
+
+        [Test]
+        public void testFirstFind()
+        {
+            Parser p = new Parser();
+            string HTML = "<h1> < a href = class=\"result-link\" data-pos=\"1\" data-event=\"search_click_match\" >Гарри</a> ";
+            Assert.AreEqual("\"1\" [Точное совпадение]  Гарри \r\n", p.Parsing(HTML, "data-pos=(.+)"));            
+        }
+
+        [Test]
+        public void testFullFind()
+        {
+            Parser p = new Parser();
+            string HTML = "< a href = \"http://ru.harrypotter.wikia.com/wiki/%D0%93%D0%B0%D1%80%D1%80%D0%B8\" class=\"result-link\" data-pos=\"1\" data-event=\"search_click_match\" >Гарри</a>" + Environment.NewLine +
+                "Гарри Поттер — самый знаменитый студент Хогвартса за последние сто лет., Гарри Тригг — волшебник, пропавший без вести в 1997 году.В реальном мире Гарри Меллинг — актёр, исполнивший роль Дадли Дурсля&hellip;" + Environment.NewLine +
+                "<li><a href=\"http://ru.harrypotter.wikia.com/wiki/%D0%93%D0%B0%D1%80%D1%80%D0%B8\" class=\"result-link\" data-pos=\"1\" data-event=\"search_click_match\" >http://ru.harrypotter.wikia.com/wiki/Гарри</a></li>";
+            Assert.AreEqual("\"1\" [Точное совпадение]  Гарри\r\r\n\"1\" [Точное совпадение]  http://ru.harrypotter.wikia.com/wiki/Гарри\r\n", p.Parsing(HTML, "data-pos=(.+)"));
+        }
+
+        [Test]
+        public void testTwoFinds()
+        {
+            Parser p = new Parser();
+            string HTML = "< a href = \"http://ru.harrypotter.wikia.com/wiki/%D0%93%D0%B0%D1%80%D1%80%D0%B8\" class=\"result-link\" data-pos=\"1\" data-event=\"search_click_match\" >Гарри</a>" + Environment.NewLine +
+                "Гарри Поттер — самый знаменитый студент Хогвартса за последние сто лет., Гарри Тригг — волшебник, пропавший без вести в 1997 году.В реальном мире Гарри Меллинг — актёр, исполнивший роль Дадли Дурсля&hellip;" + Environment.NewLine +
+                "<li><a href=\"http://ru.harrypotter.wikia.com/wiki/%D0%93%D0%B0%D1%80%D1%80%D0%B8\" class=\"result-link\" data-pos=\"1\" data-event=\"search_click_match\" >http://ru.harrypotter.wikia.com/wiki/Гарри</a></li>";
+            Assert.AreEqual("\"1\" [Точное совпадение]  Гарри\r\r\n\"1\" [Точное совпадение]  http://ru.harrypotter.wikia.com/wiki/Гарри\r\n", p.Parsing(HTML, "data-pos=(.+)"));
+        }
+
+    }
+}
